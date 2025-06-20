9d6767454222b2f1378d30c2c871601bd242ecd3
fran <fran@Macario.local>
2025-06-20 18:24:19 +0200

    Minor changes

--- GenerateMultiFrameData.py
+++ GenerateMultiFrameData.py
@@ -5,28 +5,31 @@ Created on Mon May 19 17:43:35 2025
 
 @author: fran
 """
+import os
 
 from utils.data_processing import (
     create_labeled_sequences_from_annotations
 )
 
-video_directory = 'videos'  # Replace with your video directory
-annotation_directory = 'dataframes' # Replace with your dataframe directory
-output_directory = 'labeled_sequences'
-sequence_length = 10
-stride = 5
+DATASET_TYPE = 'drones' # 'cars' # 
+
+sequence_length = 2
+stride = 1
 target_size = (224, 224)
 train_test_ratio = 0.8
+output_directory = os.path.join(DATASET_TYPE, f'labeled_sequences_len_{sequence_length}_stride_{stride}')
 
 train_dir, test_dir, class_names = create_labeled_sequences_from_annotations(
-    video_directory,
-    annotation_directory,
-    output_directory,
-    sequence_length=sequence_length,
-    target_size=target_size,
-    train_test_split_ratio=train_test_ratio,
-    stride = stride
-)
+    dataset_type = DATASET_TYPE,
+    base_dataset_dir = DATASET_TYPE, # e.g., 'drones', 'cars'
+    output_dir = output_directory, 
+    sequence_length = sequence_length, 
+    target_size = target_size, 
+    train_test_split_ratio = train_test_ratio, 
+    stride = stride,
+    num_collision_videos_to_process = 25, # For 'cars' dataset to limit processing
+    num_no_collision_videos_to_process = 15 # For 'cars' dataset to limit processing
+) 
 
 print(f"Training sequences saved to: {train_dir}")
 print(f"Testing sequences saved to: {test_dir}")
